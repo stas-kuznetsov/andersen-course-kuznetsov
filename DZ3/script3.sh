67b22c7ecdf30c6f9a091ac9da74887c1bc433b0
Stanislav Kuznetsov <o937112740@gmail.com>
2021-09-13 09:21:28 +0300

    correct DZ3

--- DZ3/script3.sh
+++ DZ3/script3.sh
@@ -6,5 +6,5 @@ read ent
 echo 'How maximum lines do you want to output?'
 read lines
 
-netstat -tunapl 2>/dev/null | awk '$ent {print $5}' | cut -d: -f1 | sort | uniq -c | sort | tail -n$lines | grep -oP '(\d+\.){3}\d+'| while read IP ; do whois $IP | awk -F':' '/^Organization/ {print $2}' ; done
+netstat -tunapl 2>/dev/null | awk '"/"$ent {print $5}' | cut -d: -f1 | sort | uniq -c | sort | tail -n$lines | grep -oP '(\d+\.){3}\d+'| while read IP ; do whois $IP | awk -F':' '/^Organization/ {print $2}' ; done
 
