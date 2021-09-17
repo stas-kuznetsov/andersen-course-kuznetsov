a3226397f6681ee2ac482431ab35c1e5970259b6
Stanislav Kuznetsov <o937112740@gmail.com>
2021-09-17 17:00:32 +0300

    file DZ2

---
+++ DZ2/dz2.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask import request
+from flask import url_for
+from flask import json
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def dz2():
+        if request.method == 'GET':
+                return 'Hello. This site created by Stanislav Kuznetsov 😀  :cow: '
+        else:
+                request_data = request.get_json(force=True)
+
+                if "animal" in request_data:
+                        animal = request_data["animal"]
+
+                if 'sound' in request_data:
+                        sound = request_data["sound"]
+
+                if  'count' in request_data:
+                        count = request_data["count"]
+
+                i=0
+                list = ''
+
+                while i < count:
+                        list = list + animal + ' says ' + sound + '\n'
+                        i += 1
+
+                list = list + 'Made with :' + animal + ': by Stanislav Kuznetsov \n'
+
+                return list
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
+
