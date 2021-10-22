eb6b08fce352d591f954d2036e16c2e32664c8b1
Stanislav Kuznetsov <o937112740@gmail.com>
2021-10-22 09:49:51 +0300

    replace dz2.py

--- DZ2/dz2.py
+++ DZ2/dz2.py
@@ -2,40 +2,44 @@ from flask import Flask
 from flask import request
 from flask import url_for
 from flask import json
+import emoji
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def dz2():
-        if request.method == 'GET':
-                return 'Hello. This site created by Stanislav Kuznetsov 😀  :cow: '
-        else:
-                request_data = request.get_json(force=True)
+	if request.method == 'GET':
+		return 'Hello. This site created by Stanislav Kuznetsov 😀 '
+	else:
+		request_data = request.get_json(force=True)
 
-                if "animal" in request_data:
-                        animal = request_data["animal"]
+		if "animal" in request_data:
+			animal = request_data["animal"]
 
-                if 'sound' in request_data:
-                        sound = request_data["sound"]
+		if 'sound' in request_data:
+			sound = request_data["sound"]
 
-                if  'count' in request_data:
-                        count = request_data["count"]
+		if  'count' in request_data:
+			count = request_data["count"]
 
-                i=0
-                list = ''
+		i=0
+		list = ''
 
-                while i < count:
-                        list = list + animal + ' says ' + sound + '\n'
-                        i += 1
+		while i < count:
+			list = list + animal + ' says ' + sound + '\n'
+			i += 1
 
-                list = list + 'Made with :' + animal + ': by Stanislav Kuznetsov \n'
+		pict = emoji.emojize(f':{animal}:')
+		list = list + 'Made with ' + pict  + ' by Stanislav Kuznetsov \n'
 
-                return list
+		return list
 
 
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
 
-   
+
+
+
